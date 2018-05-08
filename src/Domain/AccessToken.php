d25a0912353e6c9f053d68df0f79020bd4769056
Gemma Black <gemma.black@breakthroughmedia.org>
2018-05-08 13:31:26 +0100

    Access Token domain type created

---
+++ src/Domain/AccessToken.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wcom\Jwt\Domain;
+
+use Exception;
+
+class AccessToken
+{
+
+    private $token;
+
+    public static function define($accessToken)
+    {
+        if (!is_string($accessToken)) {
+            throw new Exception('Access token should be a string');
+        }
+
+        if (!self::isJWTStructure($accessToken)) {
+            throw new Exception('Access token is not a valid JWT');
+        }
+
+        return new static($accessToken);
+    }
+
+    private static function isJWTStructure($value)
+    {
+        $testing = explode('.', $value);
+
+        if (count($testing) !== 3) {
+            return false;
+        }
+
+        $shortParts = array_filter($testing, function($value) {
+            return strlen($value) <= 8;
+        });
+
+        return count($shortParts) === 0;
+    }
+
+    private function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function __toString()
+    {
+        return $this->token;
+    }
+}
---
+++ tests/Domain/AccessTokenTest.php
@@ -0,0 +1,63 @@
+<?php 
+
+use Wcom\Jwt\Domain\AccessToken;
+use PHPUnit\Framework\TestCase;
+use Eris\Generator;
+use Eris\TestTrait;
+
+/**
+ * Access Token test
+ * @author Gemma Black <gblackuk@googlemail.com>
+ */
+class AccessTokenTest extends TestCase
+{
+
+    use TestTrait;
+
+    /**
+     * Example JWT
+     * @link https://scotch.io/tutorials/the-anatomy-of-a-json-web-token
+     * @var string
+     */
+    public $jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzY290Y2guaW8iLCJleHAiOjEzMDA4MTkzODAsIm5hbWUiOiJDaHJpcyBTZXZpbGxlamEiLCJhZG1pbiI6dHJ1ZX0.03f329983b86f7d9a9f5fef85305880101d5e302afafa20154d094b229f75773';
+    public $tooShortJwt = 'cq7~ir-/@6H1/~}B[eb#b7=Ap|-"5c_5>K~_.vIf^;)6Q!I<Tu:)<s19@r#Hb"&a4k`k$C&l8167?\e6gRH_:"5YtOH;.~h<f]<Sz';
+
+    public function test_sets_access_token()
+    {
+        $token = AccessToken::define($this->jwt);
+        $this->assertEquals($this->jwt, $token);
+    }
+
+    public function test_only_accepts_strings1()
+    {
+        $this
+            ->forAll(
+                Generator\int()
+            )
+            ->then(function($int) {
+                try {
+                    AccessToken::define($int);
+                    $this->fail('This call should raise exception! ' . $int);
+                } catch (Exception $e) {
+                    $this->assertEquals(
+                        'Access token should be a string',
+                        $e->getMessage()
+                    );
+                }
+            });
+    }
+
+    public function test_only_accepts_string2()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Access token should be a string');
+        AccessToken::define([]);
+    }
+    
+    public function test_only_accepts_properly_formatted_strings()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Access token is not a valid JWT');
+        AccessToken::define($this->tooShortJwt);
+    }
+}
